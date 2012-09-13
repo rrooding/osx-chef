16c1780465e07f192e32efee65090be3cc481d0c
Ralph Rooding <ralph@izerion.com>
2012-09-13 23:28:15 +0200

    Added mysql install db command

--- chef/cookbooks/mysql/recipes/default.rb
+++ chef/cookbooks/mysql/recipes/default.rb
@@ -1,1 +1,6 @@
 package 'mysql'
+
+execute 'Installing default mysql databases' do
+  command 'mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp'
+  not_if { File.exist? '/usr/local/var/mysql' }
+end
