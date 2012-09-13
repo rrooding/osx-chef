40bf165ed6c9cf5d3622a45c081b61a4ba9066f8
Ralph Rooding <ralph@izerion.com>
2012-09-13 23:50:08 +0200

    Added copy of plist for mysql

--- chef/cookbooks/mysql/recipes/default.rb
+++ chef/cookbooks/mysql/recipes/default.rb
@@ -1,6 +1,16 @@
+require 'pathname'
+
 package 'mysql'
 
 execute 'Installing default mysql databases' do
-  command 'mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp'
+  command 'mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp >/dev/null'
   not_if { File.exist? '/usr/local/var/mysql' }
+end
+
+ruby_block 'copy mysql plist to ~/Library/LaunchAgents' do
+  active_mysql = Pathname.new("/usr/local/bin/mysql").realpath
+  plist_location = File.expand_path(File.join(active_mysql, '../../', 'homebrew.mxcl.mysql.plist'))
+  plist_destination = File.expand_path(File.join('~', 'Library', 'LaunchAgents'))
+  system "cp #{plist_location} #{plist_destination}"
+  not_if { File.exists? plist_destination }
 end
