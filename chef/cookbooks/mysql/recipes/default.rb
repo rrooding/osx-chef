8b0d8d42dcee991fcb8bfba05209947a22de1c5c
Ralph Rooding <ralph@izerion.com>
2012-09-17 11:03:51 +0200

    Added mysql service

--- chef/cookbooks/mysql/recipes/default.rb
+++ chef/cookbooks/mysql/recipes/default.rb
@@ -4,13 +4,29 @@ package 'mysql'
 
 execute 'Installing default mysql databases' do
   command 'mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp >/dev/null'
+
   not_if { File.exist? '/usr/local/var/mysql' }
 end
 
 ruby_block 'copy mysql plist to ~/Library/LaunchAgents' do
-  active_mysql = Pathname.new("/usr/local/bin/mysql").realpath
-  plist_location = File.expand_path(File.join(active_mysql, '../../', 'homebrew.mxcl.mysql.plist'))
-  plist_destination = File.expand_path(File.join('~', 'Library', 'LaunchAgents', 'homebrew.mxcl.mysql.plist'))
-  system "cp #{plist_location} #{plist_destination}"
+
+  block do
+    active_mysql = Pathname.new("/usr/local/bin/mysql").realpath
+    plist_location = File.expand_path(File.join(active_mysql, '../../', 'homebrew.mxcl.mysql.plist'))
+    system "cp #{plist_location} #{plist_destination}"
+  end
+
   not_if { File.exists? plist_destination }
+end
+
+service 'mysql' do
+  service_name 'homebrew.mxcl.mysql'
+  supports :status => true
+  action [ :enable, :start ]
+
+  only_if { File.exists? plist_destination }
+end
+
+def plist_destination
+  File.expand_path(File.join('~', 'Library', 'LaunchAgents', 'homebrew.mxcl.mysql.plist'))
 end
