f5d00cd9f53134e083fa5b07ad2e8e9c21b6b900
Ralph Rooding <ralph@izerion.com>
2012-09-18 00:48:14 +0200

    Fixed rvm

--- chef/cookbooks/rvm/recipes/default.rb
+++ chef/cookbooks/rvm/recipes/default.rb
@@ -2,8 +2,3 @@ execute 'Install RVM' do
   command 'curl -L https://get.rvm.io | bash -s stable'
   not_if { File.directory?("#{ENV['HOME']}/.rvm/") }
 end
-
-execute 'Upgrade RVM' do
-  command 'rvm get head'
-  only_if { File.directory?("#{ENV['HOME']}/.rvm/") }
-end
