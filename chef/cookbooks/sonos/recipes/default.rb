7fee40ef7c9fa122445cfbc9f649cacd15bb6706
Ralph Rooding <ralph@izerion.com>
2012-09-29 16:41:50 +0200

    Typo

--- chef/cookbooks/sonos/recipes/default.rb
+++ chef/cookbooks/sonos/recipes/default.rb
@@ -2,6 +2,6 @@ dmg_package "Sonos Desktop Controller" do
   dmg_name "SonosDesktopController#{node['sonos']['version']}"
   source "http://update.sonos.com/software/mac/mdcr/SonosDesktopController#{node['sonos']['version']}.dmg"
   checksum node['sonos']['checksum']
-  accept_uela true
+  accept_eula true
   not_if { File.directory?("/Applications/Sonos.app") }
 end
