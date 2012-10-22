b8fbffe5f3417bc4fb4813274157682d62b78e27
Ralph Rooding <ralph@izerion.com>
2012-10-22 13:51:32 +0200

    Added janus plugins

--- chef/cookbooks/vim/recipes/default.rb
+++ chef/cookbooks/vim/recipes/default.rb
@@ -4,10 +4,16 @@ execute 'curl -Lo- https://bit.ly/janus-bootstrap | bash' do
   creates "#{ENV['HOME']}/.vim/janus/"
 end
 
-template "#{ENV['HOME']}/.vimrc.before" do
-  source 'vimrc.before.erb'
+%w{vimrc.before vimrc.after}.each do |tpl|
+  template "#{ENV['HOME']}/.#{tpl}" do
+    source "#{tpl}.erb"
+  end
 end
 
-template "#{ENV['HOME']}/.vimrc.after" do
-  source 'vimrc.after.erb'
+{ 'git://github.com/Lokaltog/vim-powerline.git' => 'vim-powerline',
+  'git://github.com/altercation/vim-colors-solarized.git' => 'vim-colors-solarized',
+  'git://github.com/tpope/vim-unimpaired.git' => 'vim-unimpaired'}.each do |plugin, path|
+  execute "git clone #{plugin} #{ENV['HOME']}/.janus/#{path}" do
+    creates "#{ENV['HOME']}/.janus/#{path}"
+  end
 end
