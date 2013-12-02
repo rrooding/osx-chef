2f3dc0839f660528cc8c2947d3c3074d0b261300
Ralph Rooding <ralph@Ralph-Roodings-MacBook-Pro.local>
2013-12-03 00:53:12 +0100

    Added vim config for fish

--- workstation/recipes/janus.rb
+++ workstation/recipes/janus.rb
@@ -1,3 +1,7 @@
 execute 'curl -Lo- https://bit.ly/janus-bootstrap | bash' do
-  creates "#{ENV['HOME']}/.vim/janus/"
+  creates "#{node['sprout']['home']}/.vim/janus/"
+end
+
+template "#{node['sprout']['home']}/.vimrc.before" do
+  source "vimrc.before"
 end
