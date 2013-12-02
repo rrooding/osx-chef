78650b962815aad59723482002f36f208f5477cf
Ralph Rooding <ralph@Ralph-Roodings-MacBook-Pro.local>
2013-12-02 21:20:10 +0100

    Set fish as default shell

--- workstation/recipes/fish_shell.rb
+++ workstation/recipes/fish_shell.rb
@@ -4,3 +4,8 @@ execute "Add fish to /etc/shells" do
   command "echo '/usr/local/bin/fish' | sudo tee -a /etc/shells"
   not_if "grep -q fish /etc/shells"
 end
+
+execute "Set fish as default shell" do
+  command "chsh -s /usr/local/bin/fish"
+  if "grep -q fish /etc/shells"
+end
