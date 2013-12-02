97d6eb32a96c2f6e60709b6b977a434867cb6a26
Ralph Rooding <ralph@Ralph-Roodings-MacBook-Pro.local>
2013-12-02 23:15:32 +0100

    Rubies

--- workstation/recipes/rubies.rb
+++ workstation/recipes/rubies.rb
@@ -1,1 +1,13 @@
 brew "chruby"
+
+%w{bash zsh}.each do |shell|
+  execute "Add chruby to #{shell}" do
+    command "echo 'source /usr/local/opt/chruby/share/chruby/chruby.sh' | tee -a #{node['sprout']['home']}/.#{shell}rc"
+    not_if "grep -q chruby #{node['sprout']['home']}/.#{shell}rc"
+  end
+
+  execute "Add chruby autoloading to #{shell}" do
+    command "echo 'source /usr/local/opt/chruby/share/chruby/auto.sh' | tee -a #{node['sprout']['home']}/.#{shell}rc"
+    not_if "grep -q chruby #{node['sprout']['home']}/.#{shell}rc"
+  end
+end
