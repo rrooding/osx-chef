1290abc7e36bddee47e8ba9775e05b921a1b123e
Ralph Rooding <ralph@Ralph-Roodings-MacBook-Pro.local>
2013-12-02 23:55:40 +0100

    Rubies

--- workstation/recipes/rubies.rb
+++ workstation/recipes/rubies.rb
@@ -21,6 +21,16 @@ remote_file "/usr/local/opt/chruby/share/chruby/auto.fish" do
   source "https://gist.github.com/rrooding/7760489/raw/cbf1766e8f546c4be6c7d3be7f678070f94824ec/fish-chruby-auto.fish"
 end
 
+execute "Add chruby to fish" do
+  command "echo 'source /usr/local/opt/chruby/share/chruby/chruby.fish' | tee -a #{node['sprout']['home']}/.config/fish/config.fish"
+  not_if "grep -q chruby #{node['sprout']['home']}/.config/fish/config.fish"
+end
+
+execute "Add chruby autoloading to fish" do
+  command "echo 'source /usr/local/opt/chruby/share/chruby/auto.fish' | tee -a #{node['sprout']['home']}/.config/fish/config.fish"
+  not_if "grep -q auto #{node['sprout']['home']}/.config/fish/config.fish"
+end
+
 node['rubies']['list'].each do |ruby|
   execute "Installing #{ruby}" do
     command "ruby-install #{ruby}"
