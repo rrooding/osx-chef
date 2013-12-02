1b5e91909dd53afb8ce73ad9f2c538235cf65f41
Ralph Rooding <ralph@Ralph-Roodings-MacBook-Pro.local>
2013-12-02 22:32:56 +0100

    Make fish config dir

--- workstation-settings/recipes/fish.rb
+++ workstation-settings/recipes/fish.rb
@@ -1,3 +1,7 @@
+directory "#{node['sprout']['home']}/.config/fish/functions" do
+  recursive true
+end
+
 %w{fish_prompt}.each do |fish_config|
   template "#{node['sprout']['home']}/.config/fish/functions/#{fish_config}.fish" do
     source "#{fish_config}.fish.erb"
