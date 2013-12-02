5d46373c96c6cd654a5fc413d3ca389395d3fcde
Ralph Rooding <ralph@Ralph-Roodings-MacBook-Pro.local>
2013-12-03 00:19:02 +0100

    Remove fish default message

--- workstation-settings/recipes/fish.rb
+++ workstation-settings/recipes/fish.rb
@@ -2,6 +2,10 @@ directory "#{node['sprout']['home']}/.config/fish/functions" do
   recursive true
 end
 
+template "#{node['sprout']['home']}/.config/fish/config.fish" do
+  source "config.fish.erb"
+end
+
 %w{fish_prompt}.each do |fish_config|
   template "#{node['sprout']['home']}/.config/fish/functions/#{fish_config}.fish" do
     source "#{fish_config}.fish.erb"
