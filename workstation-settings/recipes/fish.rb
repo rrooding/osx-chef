24b56ac40f7fabc7cecc62ebf0f451f4ba2d6baf
Ralph Rooding <ralph@ralphrooding.com>
2013-12-04 09:32:40 +0100

    Fixed typo in fish function and added user

--- workstation-settings/recipes/fish.rb
+++ workstation-settings/recipes/fish.rb
@@ -1,19 +1,23 @@
 %w{functions plugins}.each do |dir|
   directory "#{node['sprout']['home']}/.config/fish/#{dir}" do
     recursive true
+    user node['current_user']
   end
 end
 
 template "#{node['sprout']['home']}/.config/fish/config.fish" do
   source "config.fish.erb"
+  user node['current_user']
 end
 
 template "#{node['sprout']['home']}/.config/fish/functions/fish_prompt.fish" do
   source "fish/functions/fish_prompt.fish.erb"
+  user node['current_user']
 end
 
 %w{git heroku bundler vim}.each do |fish_config|
   template "#{node['sprout']['home']}/.config/fish/plugins/#{fish_config}.fish" do
     source "fish/plugins/#{fish_config}.fish.erb"
+    user node['current_user']
   end
 end
